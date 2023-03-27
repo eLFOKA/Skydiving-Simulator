7c80572769819bc749c8ee8f46f1caf1ae5cc127
mimers19 <mimers19@gmail.com>
2023-03-28 01:17:49 +0200

    get numeric value

--- simulator/src/GUI.java
+++ simulator/src/GUI.java
@@ -1,21 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import static javax.swing.JOptionPane.showMessageDialog;
 
-public class GUI implements ActionListener {                           //Creating class GUI (Guide User Interface)
+public class GUI {                           //Creating class GUI (Guide User Interface)
     JFrame frame;                            //Creating a window inside GUI
     JPanel settings_panel;                   //Creating panel inside GUI for adjusting variables
     JPanel animation_panel;                  //Creating panel inside GUI for showing animations
     JTextField txt_field_m;
     JTextField txt_field_b;
     JTextField txt_field_y0;
-    JButton m_button;
-    JButton b_button;
-    JButton y0_button;
-    JButton run_button;
+    public JButton run_button;
 
-    public GUI(){
+    public GUI() {
         frame = new JFrame();
         settings_panel = new JPanel();
         animation_panel = new JPanel();
@@ -40,23 +36,19 @@ public class GUI implements ActionListener {                           //Creatin
         settings_panel.setBounds(0, 0, 200, frame.getHeight());
 
         animation_panel.setBackground(main_color);
-        animation_panel.setBounds(200, 0, frame.getWidth()-200, frame.getHeight());
+        animation_panel.setBounds(200, 0, frame.getWidth() - 200, frame.getHeight());
 
         SwingUtilities.updateComponentTreeUI(frame);
     }
 
-    private void InitializePanels(){
+    private void InitializePanels() {
         Dimension dimension_text_field = new Dimension(150, 30);
-        Dimension dimension_buttons = new Dimension(30,30);
 
-        ImageIcon ok_icon = new ImageIcon("simulator/Resources/check.png");
         ImageIcon run_icon = new ImageIcon("simulator/Resources/run.png");
 
-        Image img = ok_icon.getImage();
-        Image newimg = img.getScaledInstance( 30, 30,  java.awt.Image.SCALE_SMOOTH ) ;
-        ok_icon = new ImageIcon(newimg);
 
-        img = run_icon.getImage();
+        Image img = run_icon.getImage();
+        Image newimg;
         newimg = img.getScaledInstance(60, 40, Image.SCALE_SMOOTH);
         run_icon = new ImageIcon(newimg);
 
@@ -69,14 +61,6 @@ public class GUI implements ActionListener {                           //Creatin
         txt_field_m.setPreferredSize(dimension_text_field);
         settings_panel.add(txt_field_m);
 
-        m_button = new JButton();
-        m_button.setIcon(ok_icon);
-        m_button.setBorderPainted(false);
-        m_button.setMargin(new Insets(0, 0, 0, 0));
-        m_button.setContentAreaFilled(false);
-        m_button.setPreferredSize(dimension_buttons);
-        m_button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        settings_panel.add(m_button);
 
         JLabel label_b = new JLabel("Air resistance: ");
         settings_panel.add(label_b);
@@ -85,15 +69,6 @@ public class GUI implements ActionListener {                           //Creatin
         txt_field_b.setPreferredSize(dimension_text_field);
         settings_panel.add(txt_field_b);
 
-        b_button = new JButton();
-        b_button.setIcon(ok_icon);
-        b_button.setBorderPainted(false);
-        b_button.setMargin(new Insets(0, 0, 0, 0));
-        b_button.setContentAreaFilled(false);
-        b_button.setPreferredSize(dimension_buttons);
-        b_button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        settings_panel.add(b_button);
-
         JLabel label_y0 = new JLabel("Start height: ");
         settings_panel.add(label_y0);
 
@@ -101,14 +76,6 @@ public class GUI implements ActionListener {                           //Creatin
         txt_field_y0.setPreferredSize(dimension_text_field);
         settings_panel.add(txt_field_y0);
 
-        y0_button = new JButton();
-        y0_button.setIcon(ok_icon);
-        y0_button.setBorderPainted(false);
-        y0_button.setMargin(new Insets(0, 0, 0, 0));
-        y0_button.setContentAreaFilled(false);
-        y0_button.setPreferredSize(dimension_buttons);
-        y0_button.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        settings_panel.add(y0_button);
 
         run_button = new JButton();
         run_button.setIcon(run_icon);
@@ -121,9 +88,16 @@ public class GUI implements ActionListener {                           //Creatin
 
     }
 
+    public int get_m() {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        String m = txt_field_m.getText();
 
+        try {
+            int m_int = Integer.parseInt(m);
+            return m_int;
+        } catch (NumberFormatException e) {
+            showMessageDialog(null, "Value of mass is not given in correct way. Please, use only numbers - no whitespaces or letters");
+            return 0;
+        }
     }
 }

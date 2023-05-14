760bcaba15a6c7c004825591afbd5886e1d80cc3
mimers19 <mimers19@gmail.com>
2023-05-14 15:12:05 +0200

    Zmiana technologii

--- simulator/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        
-        GUI gui = new GUI();
-        Run run = new Run(gui);
-        gui.runButton.addActionListener(run);
-    }
-}
